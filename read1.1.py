acf9c4f333bf2ef17ec8b9f136cd364ab5b816b2
napier40401632 <38060409+napier40401632@users.noreply.github.com>
2018-05-04 09:38:05 +0630

    tutorial 2

---
+++ read1.1.py
@@ -0,0 +1,11 @@
+import urllib2,json,base64
+accesstoken = "LAF4MNPN2J57W4MRUTT9"
+url = "http://data.unistats.ac.uk/api/v4/KIS/Institution/10003270.json"
+request = urllib2.Request(url)
+request.add_header(
+	"Authorization",
+	"Basic " + base64.encodestring(accesstoken+":").replace('\n','')
+	)
+response = urllib2.urlopen(request)
+data = json.load(response)
+print data['Name']
---
+++ read5.1.py
@@ -0,0 +1,22 @@
+import urllib2,json,base64
+accesstoken = "LAF4MNPN2J57W4MRUTT9"
+institution = "10007772"
+course = "U56119"
+page = 0
+url = "http://data.unistats.ac.uk/api/v4/KIS/Institution/{}/Course/{}/FullTime/Statistics.json?pageIndex={}".format(
+    institution,
+    course,
+    page
+    )
+request = urllib2.Request(url)
+request.add_header(
+	"Authorization",
+	"Basic " + base64.encodestring(accesstoken+":").replace('\n','')
+	)
+response = urllib2.urlopen(request)
+data = json.load(response)
+x = data[6]
+a = x["Details"]
+c = a[4]
+print c["Value"]
+
